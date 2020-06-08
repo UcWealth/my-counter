b37f59c1ae25cde3625a4af80484e076ce5f2ecd
ucwealth <ucheokeke16@gmail.com>
2020-06-08 22:05:44 +0100

    added component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const add = () => setCount(count + 1);
+  const minus = () => setCount(count - 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{count}</h2>
+      <button onClick={add}>Increase</button>
+      <button onClick={minus}>Reduce</button>
     </div>
   );
 }
